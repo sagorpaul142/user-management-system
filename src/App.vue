5b5d798aef434f1fa37a0cb14e52eded872266f6
sagorpaul142 <sagorpaulcse@gmail.com>
2022-10-01 23:58:56 +0600

    chore: All dependencies install and configuration in the project

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,23 @@
 <template>
-  <div class=""></div>
+  <v-app>
+    <v-main>
+      <HelloWorld/>
+    </v-main>
+  </v-app>
 </template>
 
 <script>
+import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
-  components: {}
+
+  components: {
+    HelloWorld,
+  },
+
+  data: () => ({
+    //
+  }),
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
